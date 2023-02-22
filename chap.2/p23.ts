bc92cbf4a67f6df9512dbeb7505ce585fcbb1912
oouuyab <oouuyab@gmail.com>
2023-02-23 02:05:27 +0900

    DOCS: [Chap.2] Scope 설정으로 Naming 중복을 피함

--- chap.2/p23.ts
+++ chap.2/p23.ts
@@ -1,16 +1,18 @@
-class Gear23 {
-  private _chainRing: number;
-  private _cog: number;
+{
+  class Gear {
+    private _chainRing: number;
+    private _cog: number;
 
-  constructor(chainRing: number, cog: number) {
-    this._chainRing = chainRing;
-    this._cog = cog;
+    constructor(chainRing: number, cog: number) {
+      this._chainRing = chainRing;
+      this._cog = cog;
+    }
+
+    ratio(): number {
+      return this._chainRing / this._cog;
+    }
   }
 
-  ratio(): number {
-    return this._chainRing / this._cog;
-  }
+  console.log(new Gear(52, 11).ratio());
+  console.log(new Gear(30, 27).ratio());
 }
-
-console.log(new Gear23(52, 11).ratio());
-console.log(new Gear23(30, 27).ratio());
--- chap.2/p24-p25.ts
+++ chap.2/p24-p25.ts
@@ -1,27 +1,29 @@
-class Gear24 {
-  private _chainRing: number;
-  private _cog: number;
-  private _rim: number;
-  private _tire: number;
+{
+  class Gear {
+    private _chainRing: number;
+    private _cog: number;
+    private _rim: number;
+    private _tire: number;
 
-  constructor(chainRing: number, cog: number, rim: number, tire: number) {
-    this._chainRing = chainRing;
-    this._cog = cog;
-    this._rim = rim;
-    this._tire = tire;
+    constructor(chainRing: number, cog: number, rim: number, tire: number) {
+      this._chainRing = chainRing;
+      this._cog = cog;
+      this._rim = rim;
+      this._tire = tire;
+    }
+
+    ratio(): number {
+      return this._chainRing / this._cog;
+    }
+
+    gearInches(): number {
+      return this.ratio() * (this._rim + this._tire * 2);
+    }
   }
 
-  ratio(): number {
-    return this._chainRing / this._cog;
-  }
+  console.log(new Gear(52, 11, 26, 1.5).gearInches());
+  console.log(new Gear(52, 11, 24, 1.25).gearInches());
 
-  gearInches(): number {
-    return this.ratio() * (this._rim + this._tire * 2);
-  }
+  // ! 다음 코드는 에러가 발생한다. (이전에 작성한 코드)
+  console.log(new Gear(52, 11).ratio());
 }
-
-console.log(new Gear24(52, 11, 26, 1.5).gearInches());
-console.log(new Gear24(52, 11, 24, 1.25).gearInches());
-
-// ! 다음 코드는 에러가 발생한다. (이전에 작성한 코드)
-console.log(new Gear24(52, 11).ratio());
